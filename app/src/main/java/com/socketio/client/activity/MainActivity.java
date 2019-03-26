7dc2434b182a5c1bd62ba142aa13858704603a3d
10159757 <10159757jiang_dawei@cn.tre-inc.com>
2019-03-26 14:41:42 +0800

    Initial commit

---
+++ app/src/main/java/com/socketio/client/activity/MainActivity.java
@@ -0,0 +1,142 @@
+package com.socketio.client.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.socketio.client.R;
+import com.socketio.client.adapter.MainAdapter;
+import com.socketio.client.entity.Recommendation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_listview)
+    ListView main_listview;
+    private MainAdapter mAdapter;
+
+    private List<Recommendation> mList = new ArrayList<>();
+
+    //创建socket连接
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://192.168.0.186:8081");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        initView();
+        socketConn();
+
+        mSocket.on("borcast", onLogin);
+    }
+
+    private void initView(){
+//        mAdapter = new MainAdapter(MainActivity.this,mList);
+//        main_listview.setAdapter(mAdapter);
+//        mAdapter.notifyDataSetChanged();
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    Toast.makeText(MainActivity.this, "接收到了服务端的消息", Toast.LENGTH_SHORT).show();
+                    Bundle bundle = msg.getData();
+                    String data = bundle.getString("socket_data");
+                    try {
+                        JSONObject jsonObject = new JSONObject(data);
+                        Recommendation recommendation = new Recommendation();
+
+                        recommendation.setGetrecommendationinterval(jsonObject.getString("getrecommendationinterval"));
+                        recommendation.setGetrecommendationtime(jsonObject.getString("getrecommendationtime"));
+                        recommendation.setStarttime(jsonObject.getString("starttime"));
+                        recommendation.setTimeouttime(jsonObject.getString("timeouttime"));
+                        recommendation.setUpdatetime(jsonObject.getString("updatetime"));
+                        recommendation.setCameragroup(jsonObject.getString("cameragroup"));
+                        mList.add(recommendation);
+
+                        mAdapter = new MainAdapter(MainActivity.this,mList);
+                        main_listview.setAdapter(mAdapter);
+                        mAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSocket.off("borcast", onLogin);
+
+        //取消连接Server
+        mSocket.disconnect();
+        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.off(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+    }
+
+    //连接到Server
+    private void socketConn() {
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        mSocket.connect();
+    }
+
+    private Emitter.Listener onLogin = new Emitter.Listener() {
+        @Override
+        public void call(Object... objects) {
+            String s = (String) objects[0];
+            Bundle bundle=new Bundle();
+            Log.e("server data : ", s);
+            Message msg = new Message();
+            bundle.putString("socket_data",s);
+            msg = handler.obtainMessage();//每发送一次都要重新获取
+            msg.setData(bundle);
+            msg.what = 0;
+            handler.sendMessage(msg);
+        }
+    };
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Failed to connected...", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+}
---
+++ app/src/main/java/com/socketio/client/adapter/MainAdapter.java
@@ -0,0 +1,85 @@
+package com.socketio.client.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.socketio.client.R;
+import com.socketio.client.entity.Recommendation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Recommendation> list = new ArrayList<>();
+
+    public  MainAdapter(Context mContext, List<Recommendation> mList){
+        this.context = mContext;
+        this.list = mList;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        ViewHolder holder = null;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item, null);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        holder.getrecommendationinterval.setText(list.get(position).getGetrecommendationinterval());
+        holder.getrecommendationtime.setText(list.get(position).getGetrecommendationtime());
+        holder.starttime.setText(list.get(position).getStarttime());
+        holder.timeouttime.setText(list.get(position).getStarttime());
+        holder.updatetime.setText(list.get(position).getUpdatetime());
+        holder.cameragroup.setText(list.get(position).getCameragroup());
+
+        return view;
+    }
+
+    public class ViewHolder{
+        @BindView(R.id.timeouttime)
+        TextView timeouttime;
+        @BindView(R.id.getrecommendationtime)
+        TextView getrecommendationtime;
+        @BindView(R.id.getrecommendationinterval)
+        TextView getrecommendationinterval;
+        @BindView(R.id.updatetime)
+        TextView updatetime;
+        @BindView(R.id.starttime)
+        TextView starttime;
+        @BindView(R.id.cameragroup)
+        TextView cameragroup;
+
+        public ViewHolder(View view){
+            ButterKnife.bind(this, view);
+        }
+    }
+}
